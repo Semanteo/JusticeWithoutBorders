992b143f41a8bb117b469e2cf00221e1dc4309f6
Semanteo <simsim3374@gmail.com>
2023-05-16 19:39:05 +0200

    Update easter.js

--- js/easter.js
+++ js/easter.js
@@ -2,7 +2,9 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const  pass = urlParams.get('password')
 
-if(pass != "easter" || !pass){
+if(!pass) return;
+
+if(pass != "easter"){
     document.getElementById("accessde").style.display = "block";
 }
 else {
